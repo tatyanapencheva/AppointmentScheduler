cc43fd0f682715f851a33c47e8f0ba445a23fa9f
tatyanapencheva <74224258+tatyanapencheva@users.noreply.github.com>
2025-06-16 17:00:01 +0300

    added

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using AppointmentScheduling.DbInitializer;
+using AppointmentScheduling.Models;
+using AppointmentScheduling.Services;
+using AppointmentScheduling.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Builder;
+using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IAppointmentService, AppointmentService>();
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
+
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromDays(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddHttpContextAccessor();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseSession();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbInitializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+    dbInitializer.Initalize();
+}
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ Utility/Helper.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace AppointmentScheduling.Utility
+{
+    public static class Helper
+    {
+        public static string Admin { get; } = "Admin";
+        public static string Patient { get; } = "Patient";
+        public static string Doctor { get; } = "Doctor";
+        public static string AppointmentAdded { get; } = "Appointment added successfully.";
+        public static string AppointmentUpdated { get; } = "Appointment updated successfully.";
+        public static string AppointmentDeleted { get; } = "Appointment deleted successfully.";
+        public static string AppointmentExists { get; } = "Appointment for selected date and time already exists.";
+        public static string AppointmentNotExists { get; } = "Appointment not exists.";
+        public static string MeetingConfirm { get; } = "Meeting confirmed successfully.";
+        public static string MeetingConfirmError { get; } = "Error while confirming meeting.";
+        public static string AppointmentAddError { get; } = "Something went wrong, please try again.";
+        public static string AppointmentUpdateError { get; } = "Something went wrong, please try again.";
+        public static string SomethingWentWrong { get; } = "Something went wrong, please try again.";
+
+        public static int SuccessCode { get; } = 1;
+        public static int FailureCode { get; } = 0;
+
+        public static List<SelectListItem> GetRolesForDropDown(bool isAdmin)
+        {
+            if (isAdmin)
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Value = Helper.Admin, Text = Helper.Admin }
+                };
+            }
+            else
+            {
+                return new List<SelectListItem>
+                {
+                    new SelectListItem { Value = Helper.Patient, Text = Helper.Patient },
+                    new SelectListItem { Value = Helper.Doctor, Text = Helper.Doctor }
+                };
+            }
+        }
+
+        public static List<SelectListItem> GetTimeDropDown()
+        {
+            List<SelectListItem> duration = new List<SelectListItem>();
+            int minutes = 30;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                int hours = minutes / 60;
+                int remainingMinutes = minutes % 60;
+                string timeText;
+
+                if (hours > 0 && remainingMinutes == 0)
+                {
+                    timeText = $"{hours} hr";
+                }
+                else if (hours > 0 && remainingMinutes > 0)
+                {
+                    timeText = $"{hours} hr {remainingMinutes} min";
+                }
+                else
+                {
+                    timeText = $"{remainingMinutes} min";
+                }
+
+                duration.Add(new SelectListItem { Value = minutes.ToString(), Text = timeText });
+
+                minutes += 30;
+            }
+
+            return duration;
+        }
+
+    }
+}
