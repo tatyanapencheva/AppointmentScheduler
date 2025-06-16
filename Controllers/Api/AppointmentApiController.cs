0b8a5867c0fe1733cc6a811da0ca022f641b2b70
tatyanapencheva <74224258+tatyanapencheva@users.noreply.github.com>
2025-06-16 16:58:31 +0300

    added

---
+++ Controllers/Api/AppointmentApiController.cs
@@ -0,0 +1,158 @@
+﻿using AppointmentScheduling.Models.ViewModels;
+using AppointmentScheduling.Services;
+using AppointmentScheduling.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Controllers.Api
+{
+    [Route("api/Appointment")]
+    [ApiController]
+    public class AppointmentApiController : ControllerBase
+    {
+        private readonly IAppointmentService _appointmentService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string loginUserId;
+        private readonly string role;
+
+        public AppointmentApiController(IAppointmentService appointmentService, IHttpContextAccessor httpContextAccessor)
+        {
+            _appointmentService = appointmentService;
+            _httpContextAccessor = httpContextAccessor;
+            loginUserId = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            role = _httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.Role);
+        }
+
+        [HttpPost]
+        [Route("SaveCalendarData")]
+        public IActionResult SaveCalendarData(AppointmentVM data)
+        {
+            CommonResponse<int> commonResponse = new CommonResponse<int>();
+            try
+            {
+                commonResponse.status = _appointmentService.AddUpdate(data).Result;
+                if (commonResponse.status == 1)
+                {
+                    commonResponse.message = Helper.AppointmentUpdated;
+                }
+                if (commonResponse.status == 2)
+                {
+                    commonResponse.message = Helper.AppointmentAdded;
+                }
+            }
+            catch (Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.FailureCode;
+            }
+
+            return Ok(commonResponse);
+        }
+
+        [HttpGet]
+        [Route("GetCalendarData")]
+        public IActionResult GetCalendarData(string doctorId)
+        {
+            CommonResponse<List<AppointmentVM>> commonResponse = new CommonResponse<List<AppointmentVM>>();
+            try
+            {
+                if (role == Helper.Patient)
+                {
+                    commonResponse.dataenum = _appointmentService.PatientsEventsById(loginUserId);
+                    commonResponse.status = Helper.SuccessCode;
+                }
+                else if (role == Helper.Doctor)
+                {
+                    commonResponse.dataenum = _appointmentService.DoctorsEventsById(loginUserId);
+                    commonResponse.status = Helper.SuccessCode;
+                }
+                else
+                {
+                    commonResponse.dataenum = _appointmentService.DoctorsEventsById(doctorId);
+                    commonResponse.status = Helper.SuccessCode;
+                }
+            }
+            catch (Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.FailureCode;
+            }
+            return Ok(commonResponse);
+        }
+
+        [HttpGet]
+        [Route("GetCalendarDataById/{id}")]
+        public IActionResult GetCalendarDataById(int id)
+        {
+            CommonResponse<AppointmentVM> commonResponse = new CommonResponse<AppointmentVM>();
+            try
+            {
+
+                commonResponse.dataenum = _appointmentService.GetById(id);
+                commonResponse.status = Helper.SuccessCode;
+
+            }
+            catch (Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.FailureCode;
+            }
+            return Ok(commonResponse);
+        }
+
+        [HttpGet]
+        [Route("DeleteAppoinment/{id}")]
+        public async Task<IActionResult> DeleteAppoinment(int id)
+        {
+            CommonResponse<int> commonResponse = new CommonResponse<int>();
+            try
+            {
+                commonResponse.status = await _appointmentService.Delete(id);
+                commonResponse.message = commonResponse.status == 1 ? Helper.AppointmentDeleted : Helper.SomethingWentWrong;
+
+            }
+            catch (Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.FailureCode;
+            }
+            return Ok(commonResponse);
+        }
+
+        [HttpGet]
+        [Route("ConfirmEvent/{id}")]
+        public IActionResult ConfirmEvent(int id)
+        {
+            CommonResponse<int> commonResponse = new CommonResponse<int>();
+            try
+            {
+                var result = _appointmentService.ConfirmEvent(id).Result;
+                if (result > 0)
+                {
+                    commonResponse.status = Helper.SuccessCode;
+                    commonResponse.message = Helper.MeetingConfirm;
+                }
+                else
+                {
+
+                    commonResponse.status = Helper.FailureCode;
+                    commonResponse.message = Helper.MeetingConfirmError;
+                }
+
+            }
+            catch (Exception e)
+            {
+                commonResponse.message = e.Message;
+                commonResponse.status = Helper.FailureCode;
+            }
+            return Ok(commonResponse);
+        }
+
+
+    }
+}
